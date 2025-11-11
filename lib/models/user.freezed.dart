ca622ce0d488a4d05dd01d588811b32a981cbd05
jaboyc <jake@jlogical.com>
2025-11-11 11:29:50 -0500

    chore: Rename UserProfile to User

--- lib/models/user.freezed.dart
+++ lib/models/user.freezed.dart
@@ -3,7 +3,7 @@
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
-part of 'user_profile.dart';
+part of 'user.dart';
 
 // **************************************************************************
 // FreezedGenerator
@@ -13,22 +13,22 @@ part of 'user_profile.dart';
 T _$identity<T>(T value) => value;
 
 /// @nodoc
-mixin _$UserProfile {
+mixin _$User {
 
  BibleTranslation get translation; List<ChapterReference> get tabs; List<ChapterReference> get previouslyViewed;
-/// Create a copy of UserProfile
+/// Create a copy of User
 /// with the given fields replaced by the non-null parameter values.
 @JsonKey(includeFromJson: false, includeToJson: false)
 @pragma('vm:prefer-inline')
-$UserProfileCopyWith<UserProfile> get copyWith => _$UserProfileCopyWithImpl<UserProfile>(this as UserProfile, _$identity);
+$UserCopyWith<User> get copyWith => _$UserCopyWithImpl<User>(this as User, _$identity);
 
-  /// Serializes this UserProfile to a JSON map.
+  /// Serializes this User to a JSON map.
   Map<String, dynamic> toJson();
 
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is UserProfile&&(identical(other.translation, translation) || other.translation == translation)&&const DeepCollectionEquality().equals(other.tabs, tabs)&&const DeepCollectionEquality().equals(other.previouslyViewed, previouslyViewed));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is User&&(identical(other.translation, translation) || other.translation == translation)&&const DeepCollectionEquality().equals(other.tabs, tabs)&&const DeepCollectionEquality().equals(other.previouslyViewed, previouslyViewed));
 }
 
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -37,15 +37,15 @@ int get hashCode => Object.hash(runtimeType,translation,const DeepCollectionEqua
 
 @override
 String toString() {
-  return 'UserProfile(translation: $translation, tabs: $tabs, previouslyViewed: $previouslyViewed)';
+  return 'User(translation: $translation, tabs: $tabs, previouslyViewed: $previouslyViewed)';
 }
 
 
 }
 
 /// @nodoc
-abstract mixin class $UserProfileCopyWith<$Res>  {
-  factory $UserProfileCopyWith(UserProfile value, $Res Function(UserProfile) _then) = _$UserProfileCopyWithImpl;
+abstract mixin class $UserCopyWith<$Res>  {
+  factory $UserCopyWith(User value, $Res Function(User) _then) = _$UserCopyWithImpl;
 @useResult
 $Res call({
  BibleTranslation translation, List<ChapterReference> tabs, List<ChapterReference> previouslyViewed
@@ -56,14 +56,14 @@ $Res call({
 
 }
 /// @nodoc
-class _$UserProfileCopyWithImpl<$Res>
-    implements $UserProfileCopyWith<$Res> {
-  _$UserProfileCopyWithImpl(this._self, this._then);
+class _$UserCopyWithImpl<$Res>
+    implements $UserCopyWith<$Res> {
+  _$UserCopyWithImpl(this._self, this._then);
 
-  final UserProfile _self;
-  final $Res Function(UserProfile) _then;
+  final User _self;
+  final $Res Function(User) _then;
 
-/// Create a copy of UserProfile
+/// Create a copy of User
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? translation = null,Object? tabs = null,Object? previouslyViewed = null,}) {
   return _then(_self.copyWith(
@@ -77,8 +77,8 @@ as List<ChapterReference>,
 }
 
 
-/// Adds pattern-matching-related methods to [UserProfile].
-extension UserProfilePatterns on UserProfile {
+/// Adds pattern-matching-related methods to [User].
+extension UserPatterns on User {
 /// A variant of `map` that fallback to returning `orElse`.
 ///
 /// It is equivalent to doing:
@@ -91,10 +91,10 @@ extension UserProfilePatterns on UserProfile {
 /// }
 /// ```
 
-@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _UserProfile value)?  $default,{required TResult orElse(),}){
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _User value)?  $default,{required TResult orElse(),}){
 final _that = this;
 switch (_that) {
-case _UserProfile() when $default != null:
+case _User() when $default != null:
 return $default(_that);case _:
   return orElse();
 
@@ -113,10 +113,10 @@ return $default(_that);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _UserProfile value)  $default,){
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _User value)  $default,){
 final _that = this;
 switch (_that) {
-case _UserProfile():
+case _User():
 return $default(_that);}
 }
 /// A variant of `map` that fallback to returning `null`.
@@ -131,10 +131,10 @@ return $default(_that);}
 /// }
 /// ```
 
-@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _UserProfile value)?  $default,){
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _User value)?  $default,){
 final _that = this;
 switch (_that) {
-case _UserProfile() when $default != null:
+case _User() when $default != null:
 return $default(_that);case _:
   return null;
 
@@ -154,7 +154,7 @@ return $default(_that);case _:
 
 @optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( BibleTranslation translation,  List<ChapterReference> tabs,  List<ChapterReference> previouslyViewed)?  $default,{required TResult orElse(),}) {final _that = this;
 switch (_that) {
-case _UserProfile() when $default != null:
+case _User() when $default != null:
 return $default(_that.translation,_that.tabs,_that.previouslyViewed);case _:
   return orElse();
 
@@ -175,7 +175,7 @@ return $default(_that.translation,_that.tabs,_that.previouslyViewed);case _:
 
 @optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( BibleTranslation translation,  List<ChapterReference> tabs,  List<ChapterReference> previouslyViewed)  $default,) {final _that = this;
 switch (_that) {
-case _UserProfile():
+case _User():
 return $default(_that.translation,_that.tabs,_that.previouslyViewed);}
 }
 /// A variant of `when` that fallback to returning `null`
@@ -192,7 +192,7 @@ return $default(_that.translation,_that.tabs,_that.previouslyViewed);}
 
 @optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( BibleTranslation translation,  List<ChapterReference> tabs,  List<ChapterReference> previouslyViewed)?  $default,) {final _that = this;
 switch (_that) {
-case _UserProfile() when $default != null:
+case _User() when $default != null:
 return $default(_that.translation,_that.tabs,_that.previouslyViewed);case _:
   return null;
 
@@ -204,9 +204,9 @@ return $default(_that.translation,_that.tabs,_that.previouslyViewed);case _:
 /// @nodoc
 @JsonSerializable()
 
-class _UserProfile extends UserProfile {
-  const _UserProfile({this.translation = BibleTranslation.asv, final  List<ChapterReference> tabs = const [], final  List<ChapterReference> previouslyViewed = const []}): _tabs = tabs,_previouslyViewed = previouslyViewed,super._();
-  factory _UserProfile.fromJson(Map<String, dynamic> json) => _$UserProfileFromJson(json);
+class _User extends User {
+  const _User({this.translation = BibleTranslation.asv, final  List<ChapterReference> tabs = const [], final  List<ChapterReference> previouslyViewed = const []}): _tabs = tabs,_previouslyViewed = previouslyViewed,super._();
+  factory _User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
 @override@JsonKey() final  BibleTranslation translation;
  final  List<ChapterReference> _tabs;
@@ -224,20 +224,20 @@ class _UserProfile extends UserProfile {
 }
 
 
-/// Create a copy of UserProfile
+/// Create a copy of User
 /// with the given fields replaced by the non-null parameter values.
 @override @JsonKey(includeFromJson: false, includeToJson: false)
 @pragma('vm:prefer-inline')
-_$UserProfileCopyWith<_UserProfile> get copyWith => __$UserProfileCopyWithImpl<_UserProfile>(this, _$identity);
+_$UserCopyWith<_User> get copyWith => __$UserCopyWithImpl<_User>(this, _$identity);
 
 @override
 Map<String, dynamic> toJson() {
-  return _$UserProfileToJson(this, );
+  return _$UserToJson(this, );
 }
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is _UserProfile&&(identical(other.translation, translation) || other.translation == translation)&&const DeepCollectionEquality().equals(other._tabs, _tabs)&&const DeepCollectionEquality().equals(other._previouslyViewed, _previouslyViewed));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _User&&(identical(other.translation, translation) || other.translation == translation)&&const DeepCollectionEquality().equals(other._tabs, _tabs)&&const DeepCollectionEquality().equals(other._previouslyViewed, _previouslyViewed));
 }
 
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -246,15 +246,15 @@ int get hashCode => Object.hash(runtimeType,translation,const DeepCollectionEqua
 
 @override
 String toString() {
-  return 'UserProfile(translation: $translation, tabs: $tabs, previouslyViewed: $previouslyViewed)';
+  return 'User(translation: $translation, tabs: $tabs, previouslyViewed: $previouslyViewed)';
 }
 
 
 }
 
 /// @nodoc
-abstract mixin class _$UserProfileCopyWith<$Res> implements $UserProfileCopyWith<$Res> {
-  factory _$UserProfileCopyWith(_UserProfile value, $Res Function(_UserProfile) _then) = __$UserProfileCopyWithImpl;
+abstract mixin class _$UserCopyWith<$Res> implements $UserCopyWith<$Res> {
+  factory _$UserCopyWith(_User value, $Res Function(_User) _then) = __$UserCopyWithImpl;
 @override @useResult
 $Res call({
  BibleTranslation translation, List<ChapterReference> tabs, List<ChapterReference> previouslyViewed
@@ -265,17 +265,17 @@ $Res call({
 
 }
 /// @nodoc
-class __$UserProfileCopyWithImpl<$Res>
-    implements _$UserProfileCopyWith<$Res> {
-  __$UserProfileCopyWithImpl(this._self, this._then);
+class __$UserCopyWithImpl<$Res>
+    implements _$UserCopyWith<$Res> {
+  __$UserCopyWithImpl(this._self, this._then);
 
-  final _UserProfile _self;
-  final $Res Function(_UserProfile) _then;
+  final _User _self;
+  final $Res Function(_User) _then;
 
-/// Create a copy of UserProfile
+/// Create a copy of User
 /// with the given fields replaced by the non-null parameter values.
 @override @pragma('vm:prefer-inline') $Res call({Object? translation = null,Object? tabs = null,Object? previouslyViewed = null,}) {
-  return _then(_UserProfile(
+  return _then(_User(
 translation: null == translation ? _self.translation : translation // ignore: cast_nullable_to_non_nullable
 as BibleTranslation,tabs: null == tabs ? _self._tabs : tabs // ignore: cast_nullable_to_non_nullable
 as List<ChapterReference>,previouslyViewed: null == previouslyViewed ? _self._previouslyViewed : previouslyViewed // ignore: cast_nullable_to_non_nullable
