480e16c70a4b2152d941db6b6d18684bd60668a1
jaboyc <jake@jlogical.com>
2025-11-15 18:06:56 -0500

    feat: Add clipboard action

--- lib/style/style_context_extensions.dart
+++ lib/style/style_context_extensions.dart
@@ -1,6 +1,10 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:bible/style/color_library.dart';
 import 'package:bible/style/text_style_library.dart';
+import 'package:bible/style/widgets/component_size.dart';
+import 'package:bible/style/widgets/styled_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 extension StyleContextExtensions on BuildContext {
   Brightness get brightness => Theme.of(this).brightness;
@@ -25,5 +29,30 @@ extension StyleContextExtensions on BuildContext {
       useRootNavigator: true,
       builder: (context) => sheet,
     );
+  }
+
+  void showStyledSnackbar({required String messageText}) async {
+    Flushbar(
+      messageText: StyledListItem(
+        size: ComponentSize.sm,
+        title: Text(messageText, style: textStyle.paragraphMd.copyWith(color: colors.contentPrimaryInverse)),
+        leading: SizedBox(
+          width: 64,
+          height: 64,
+          child: Center(child: Icon(Symbols.check_circle, size: 24, color: colors.contentPrimaryInverse)),
+        ),
+      ),
+      duration: Duration(seconds: 3),
+      dismissDirection: FlushbarDismissDirection.VERTICAL,
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.zero,
+      backgroundColor: colors.inverted.surfacePrimary,
+      borderRadius: BorderRadius.circular(12),
+      shouldIconPulse: false,
+      forwardAnimationCurve: Curves.easeOutCubic,
+      reverseAnimationCurve: Curves.easeInCubic,
+      animationDuration: Duration(milliseconds: 300),
+      flushbarPosition: FlushbarPosition.TOP,
+    ).show(Navigator.of(this, rootNavigator: true).context);
   }
 }
---
+++ lib/style/widgets/component_size.dart
@@ -0,0 +1,1 @@
+enum ComponentSize { sm, md, lg }
