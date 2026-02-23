371a594b21ff9e43b46d2f8b3a8e612eea70edcf
jaboyc <jake@jlogical.com>
2026-02-22 21:42:46 -0500

    chore: Export style widgets.

--- lib/models/selection_action.dart
+++ lib/models/selection_action.dart
@@ -1,7 +1,7 @@
 import 'package:bible/models/bible.dart';
 import 'package:bible/models/reference/selection.dart';
 import 'package:bible/models/user.dart';
-import 'package:bible/style/style_context_extensions.dart';
+import 'package:bible/style/style.dart';
 import 'package:bible/ui/sheets/annotation_sheet.dart';
 import 'package:bible/utils/extensions/ref_extensions.dart';
 import 'package:flutter/material.dart';
